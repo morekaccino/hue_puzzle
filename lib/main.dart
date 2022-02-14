1ce4e7294eda8966bfc3f07432776e3f88247f9d
Mohamad Kazemi <mrkazemi95@gmail.com>
2022-02-14 01:16:17 -0500

    corners are fixed

--- lib/main.dart
+++ lib/main.dart
@@ -32,59 +32,65 @@ class _MyAppState extends State<MyApp> {
     }
   }
 
-  Widget makeWidgets({required double screenWidth, required double screenHeight}) {
+  Widget makeWidgets(
+      {required double screenWidth, required double screenHeight}) {
     double height = screenHeight / rowCount;
     double width = screenWidth / colCount;
     var colTemp = <Widget>[];
     for (int idx = 0; idx < colorMatrix.length; idx++) {
       var rowTemp = <Widget>[];
       for (int idy = 0; idy < colorMatrix[0].length; idy++) {
-        rowTemp.add(Expanded(
-          child: Draggable<List<int>>(
-            data: <int>[idx, idy],
-            child: Container(
-              width: 100,
-              height: 100,
-              color: colorMatrix[idx][idy],
-              child: DragTarget<List<int>>(
-                onAccept: (indexes) {
-                  int i = indexes[0], j = indexes[1];
-                  Color tempColor = colorMatrix[i][j];
-                  colorMatrix[i][j] = colorMatrix[idx][idy];
-                  colorMatrix[idx][idy] = tempColor;
+        if ((idx == 0 || idx == colorMatrix.length - 1) &&
+            (idy == 0 || idy == colorMatrix[0].length - 1)) {
+          rowTemp.add(Expanded(
+            child: Container(color: colorMatrix[idx][idy]),
+          ));
+        } else {
+          rowTemp.add(Expanded(
+            child: Draggable<List<int>>(
+              data: <int>[idx, idy],
+              child: Container(
+                color: colorMatrix[idx][idy],
+                child: DragTarget<List<int>>(
+                  onAccept: (indexes) {
+                    int i = indexes[0], j = indexes[1];
+                    Color tempColor = colorMatrix[i][j];
+                    colorMatrix[i][j] = colorMatrix[idx][idy];
+                    colorMatrix[idx][idy] = tempColor;
 
-                  setState(() {});
-                },
-                builder: (context, candidateData, rejectedData) {
-                  // if (candidateData.length > 0) {
-                  //   try {
-                  //     int i = candidateData[0]![0], j = candidateData[0]![1];
-                  //     return Container(
-                  //       color: colorMatrix[i][j],
-                  //     );
-                  //   } finally {}
-                  // }
-                  // return Container(
-                  //   color: Colors.transparent,
-                  // );
-                  return Container(
-                    color: colorMatrix[idx][idy],
-                  );
-                },
+                    setState(() {});
+                  },
+                  builder: (context, candidateData, rejectedData) {
+                    // if (candidateData.length > 0) {
+                    //   try {
+                    //     int i = candidateData[0]![0], j = candidateData[0]![1];
+                    //     return Container(
+                    //       color: colorMatrix[i][j],
+                    //     );
+                    //   } finally {}
+                    // }
+                    // return Container(
+                    //   color: Colors.transparent,
+                    // );
+                    return Container(
+                      color: colorMatrix[idx][idy],
+                    );
+                  },
+                ),
+              ),
+              feedback: Container(
+                width: width,
+                height: height,
+                color: colorMatrix[idx][idy],
+              ),
+              childWhenDragging: Container(
+                width: 100,
+                height: 100,
+                color: const Color(0xff1d1d1d),
               ),
             ),
-            feedback: Container(
-              width: width,
-              height: height,
-              color: colorMatrix[idx][idy],
-            ),
-            childWhenDragging: Container(
-              width: 100,
-              height: 100,
-              color: const Color(0xff1d1d1d),
-            ),
-          ),
-        ));
+          ));
+        }
       }
       colTemp.add(Expanded(
         child: Row(
